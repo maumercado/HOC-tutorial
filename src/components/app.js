baede61999819bd972eb6d27085c748f2d24cb4e
Mauricio Mercado <mau@maumercado.com>
2017-10-14 19:08:36 -0700

    Add authentication basic reducer and action

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Header from './header';
 
 class App extends Component {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import authenticationReducer from './authentication';
+
 const rootReducer = combineReducers({
-    state: (state = {}) => state
+    authenticated: authenticationReducer
 });
 
 export default rootReducer;
