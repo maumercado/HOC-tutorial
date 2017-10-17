0086aae2c94bf3639d093f49b1337fb4d8583409
Mauricio Mercado <mau@maumercado.com>
2017-10-16 23:50:48 -0700

    Remove usage of context from require_authentication hoc

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
 /**
  * in order to use this:
@@ -13,19 +12,15 @@ import PropTypes from 'prop-types';
 
 export default ComposedComponent => {
     class Authentication extends Component {
-        static contextTypes = {
-            router: PropTypes.object
-        };
-
         componentWillMount() {
             if (!this.props.authenticated) {
-                this.context.router.history.push('/');
+                this.props.history.push('/');
             }
         }
 
         componentWillUpdate(nextProps) {
             if (!nextProps.authenticated) {
-                this.context.router.history.push('/');
+                this.props.history.push('/');
             }
         }
 
